45e0998ddd78fe221ba332b6978c0638e0e5aed9
Leo Pierard <spikystickinsect@gmail.com>
2026-05-11 14:21:36 +0800

    Fixed about 3 guardian errors

--- Guardian.java
+++ Guardian.java
@@ -8,15 +8,15 @@ public class Guardian extends Character {
         this.sppMod = 0;
         this.spd += this.spdMod ;
         this.intl += this.intlMod;
-        this.atk += this.atkMod 
+        this.atk += this.atkMod;
         this.spr += this.sprMod;
         this.hlt += this.hltMod;
         this.spp += this.sppMod;
     }
 
     public boolean Special(Block[][] grid, Obstacle obstacle, int x, int y) {
-        if ( grid[x][y] == null ) {
-            grid[x][y].SetObject(2);
+        if ( grid[x][y].getEntity().getObject() == 0) {
+            grid[x][y] = new Block(obstacle);
             return true;
         }
         else {
