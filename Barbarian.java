3c8379399bfee85f27dd60bb496ecb25bcf0ffbc
Manwegongkong <lucasjanschewski@gmail.com>
2026-05-11 11:49:27 +0800

    deleted warrior, added guardian

--- Barbarian.java
+++ Barbarian.java
@@ -1,4 +1,4 @@
-public class Barbarian extends Warrior {
+public class Barbarian extends Character {
     public Barbarian() {
         super();
         this.spdMod = 2;
---
+++ Guardian.java
@@ -0,0 +1,3 @@
+public class Guardian extends Character {
+    public Guardian() {}
+}
