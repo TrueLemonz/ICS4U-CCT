d2f26be220c907ed75c4b47331ec6716e97e9514
Leo Pierard <spikystickinsect@gmail.com>
2026-05-12 12:47:08 +0800

    BSERYBGEYIAgrshiUhsyergr

---
+++ BaseCharacter.java
@@ -0,0 +1,12 @@
+public class BaseCharacter extends Character {
+    
+    public boolean Special(ActionContext context) {
+        return false;
+    }
+    public boolean Ability1(ActionContext context) {
+        return false;
+    }
+    public boolean Ability2(ActionContext context) {
+        return false;
+    }
+}
